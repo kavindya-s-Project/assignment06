0b2915a1ba740a846b2271f12be4362f5965c660
Ravindu <rtjayasooriya@gmail.com>
2022-10-27 18:55:54 +0530

    build sd card save function

--- app/src/main/java/com/example/assignment06/MainActivity.java
+++ app/src/main/java/com/example/assignment06/MainActivity.java
@@ -1,74 +1,92 @@
 package com.example.assignment06;
 
-import androidx.activity.result.contract.ActivityResultContracts;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.ContentValues;
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.drawable.BitmapDrawable;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
-import android.os.Environment;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
 
 public class MainActivity extends AppCompatActivity {
     ImageView imageView;
     Button cameraBtn;
     private Object OutputStream;
+    private Uri imageUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         imageView = findViewById(R.id.imageView);
         cameraBtn = findViewById(R.id.cameraBtn);
+
+        cameraBtn = (Button) findViewById(R.id.cameraBtn);
+        imageView = (ImageView) findViewById(R.id.imageView);
+
+        if (ContextCompat.checkSelfPermission(MainActivity.this,
+                Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this,
+                    new String[]{
+                            Manifest.permission.CAMERA
+                    },
+                    100);
+        }
 
         cameraBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivityForResult(cameraIntent,100);
-
-//                BitmapDrawable drawable = (BitmapDrawable) imageView.getDrawable();
-//                Bitmap bitmap = drawable.getBitmap();
-//
-//                File filePath = Environment.getExternalStorageDirectory();
-//                File dir = new File(filePath.getAbsolutePath() + "/Demo/");
-//                dir.mkdir();
-//                File file = new File(dir,System.currentTimeMillis() + ".jpg");
-//
-//                try {
-//                    OutputStream = new FileOutputStream(file);
-//                } catch (FileNotFoundException e) {
-//                    e.printStackTrace();
-//                }
-//                bitmap.compress(Bitmap.CompressFormat.JPEG, 100, OutputStream);
-//                try {
-//                    OutputStream.flush();
-//                }catch (IOException e) {
-//                    e.printStackTrace();
-//                }
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                Uri imagePath = createImage();
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, imagePath);
+                startActivityForResult(intent, 100);
             }
         });
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == 100) {
-            Bitmap photo = (Bitmap) data.getExtras().get("data");
-            imageView.setImageBitmap(photo);
+            if (resultCode == Activity.RESULT_OK) {
+                Toast.makeText(this, "Image captured successfully", Toast.LENGTH_SHORT)
+                        .show();
+                imageView.setImageURI(imageUri);
+            }
         }
     }
+
+    private Uri createImage() {
+        Uri uri = null;
+        ContentResolver resolver = getContentResolver();
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            uri = MediaStore.Images.Media.getContentUri(MediaStore.VOLUME_EXTERNAL_PRIMARY);
+
+        } else {
+            uri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+        }
+        String imageName = String.valueOf(System.currentTimeMillis());
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(MediaStore.Images.Media.DISPLAY_NAME, imageName + ".jpg");
+        contentValues.put(MediaStore.Images.Media.RELATIVE_PATH, "Pictures/" + "My Images/");
+        Uri finalUri = resolver.insert(uri, contentValues);
+        imageUri = finalUri;
+        return finalUri;
+    }
+
+
 }
